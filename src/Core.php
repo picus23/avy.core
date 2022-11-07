d4e8bec955fa16b6353d82e15179dfede5246d2b
Picus <picus.2022@mail.ru>
2022-11-07 14:34:41 +0300

    first

---
+++ src/Core.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Avy\core;
+
+
+class Core {
+
+    function setProps(...$props){
+        
+    }
+
+    function getData(){
+        return [
+            'active' => [],
+            'filter' => [],
+            'table'  => [],
+        ];
+    }
+
+}
